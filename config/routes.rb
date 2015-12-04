a76e99130815485586d8574525f9719742fb2a62
Per Evensen <per.evensen@gmail.com>
2015-12-03 20:40:20 -0500

    initial

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  resources :comments, except: [:new, :edit]
+  resources :posts, except: [:new, :edit]
+  resources :users, except: [:new, :edit]
+  get :token, controller: 'application'
+
+end
